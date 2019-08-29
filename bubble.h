07e71e7db8d1a9c07585675cd19652ee5a66d321
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2019-08-28 19:41:50 -0500

    BubbleSort

--- bubble.h
+++ bubble.h
@@ -8,8 +8,18 @@ class BubbleSort : public Sort {
     public:
         BubbleSort(int *elements, size_t size) : Sort(elements, size) {}
 
-        void execute() {
-            // TODO
+        void execute() override {
+            bool swapped;
+            for (int i = 0; i < size-1; ++i){
+                swapped = false;
+                for (int j = 0; j < size-i-1; ++j)
+                    if (elements[j] > elements[j+1]) {
+                        std::swap(elements[j], elements[j+1]);
+                        swapped = true;
+                    }
+                if (!swapped)
+                    break;
+            }
         }
 
         inline string name() { return "BubbleSort"; }
